174caf2607c25041d2ccddb9f7e0f5f86a8ec115
Wyatex <wyatex@qq.com>
2021-06-12 15:38:27 +0800

    添加 健康值和进度条显示

--- app/src/main/java/com/example/chainplus/fragment/MineFragment.java
+++ app/src/main/java/com/example/chainplus/fragment/MineFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.chainplus.R;
 import com.example.chainplus.viewModel.DataViewModel;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 
@@ -24,9 +27,13 @@ public class MineFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mine, container, false);
-        view.findViewById(R.id.textView15);
+        TextView health = view.findViewById(R.id.textView15);
+        ProgressBar bar = view.findViewById(R.id.progressBar);
         DataViewModel data = new ViewModelProvider(getActivity(), new ViewModelProvider.AndroidViewModelFactory(getActivity().getApplication())).get(DataViewModel.class);
-        
+        data.getHealth().observe(getActivity(), integer -> {
+            health.setText(String.valueOf(integer));
+            bar.setProgress(integer);
+        });
         return view;
     }
 }
